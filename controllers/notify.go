2d6f9fe024ac9a7059d4f3554eb412ffd36baf12
mingming <449036002@qq.com>
2018-04-08 18:20:53 +0800

    fdasfa

--- controllers/notify.go
+++ controllers/notify.go
@@ -31,7 +31,9 @@ func (c *NotifyController) OnPublish() {
 	roomName := c.GetString("name")
 	tCurl := c.GetString("tcurl")
 
-	cmd := exec.Command("ffmpeg", " -i " + tCurl + "/" + roomName ,  "-f image2",  "-ss 5", "-vframes 1",  "-s 220*220",  "/root/go/src/talkGo/static/hlsCover/" + roomName + "_cover.png")
+	fmt.Println("Cmding..............");
+	//cmd := exec.Command("ffmpeg", " -i " + tCurl + "/" + roomName ,  "-f image2",  "-ss 5", "-vframes 1",  "-s 220*220",  "/root/go/src/talkGo/static/hlsCover/" + roomName + "_cover.png")
+	cmd := exec.Command("ffmpeg", "i", tCurl + "/" + roomName ,  "f",  "image2",  "ss" , "5", "vframes",  "1",  "s",  "220*220",  "/root/go/src/talkGo/static/hlsCover/" + roomName + "_cover.png")
 	stdout, err := cmd.StdoutPipe()
 	var stderr bytes.Buffer
 	cmd.Stderr = &stderr
